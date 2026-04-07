15aa0b47a58a234edae2acf3d64bc1d0b793f84c
vincent <nexusocean@proton.me>
2026-04-06 22:53:55 -0400

    fix: 0 padding

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,13 @@
 import type { TransactionType } from '@/types/dto';
 
-export const formatFiro = (amount: number) =>
-	amount < 0.01 ? '<0.01' : Number(amount.toFixed(2)).toLocaleString();
+export const formatFiro = (amount: number) => {
+	if (amount === 0) return '0.00';
+	if (amount < 0.01) return '<0.01';
+	return amount.toLocaleString(undefined, {
+		minimumFractionDigits: 2,
+		maximumFractionDigits: 2
+	});
+};
 
 export const TX_TYPE_COLORS: Record<TransactionType, string> = {
 	transparent: 'default',
